1fb5826bdc50c7a275e4e3b58f55239341ae3f21
hoonkim <hoonk9387@gmail.com>
2022-07-31 22:43:44 -0500

    Completed assignment

--- lib/song.rb
+++ lib/song.rb
@@ -49,4 +49,40 @@ class Song
     song.save
   end
 
+  # new
+  def self.new_from_db(row)
+    # self.new is equivalent to Song.new
+    self.new(id: row[0], name: row[1], album: row[2])
+  end
+
+  # new
+  def self.all
+    # heredoc
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  # new
+  def self.find_by_name(name)
+    # heredoc
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+
+  end
+
+
 end
